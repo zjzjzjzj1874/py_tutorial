d28bd17c009c0dd2047e16a884985a03226a90ed
zjzj-py <zjzjzjzj1874@gmail.com>
2022-09-19 17:34:21 +0800

    feat✨(basic): add basic grammar
    Signed-off-by: zjzj-py <zjzjzjzj1874@gmail.com>

---
+++ tests/test.py
@@ -0,0 +1,10 @@
+print("hello python")
+
+print "hello world"
+
+# 注释:list是数组
+list = ["a", "b", 1]
+# 格式化输出py
+print("this list is {list},and len is {len}".format(list=list, len=len(list)))
+
+# https://www.cnblogs.com/fat39/p/7159881.html => 百分号和format对比
